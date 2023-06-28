20efea3ec031ab93d0bcd1a12d019e576f6704c7
MauSolorzano <127962979+MauSolorzano@users.noreply.github.com>
2023-06-28 00:13:47 -0600

    Update Crear estudiantes

--- view/estudiantes/crear.php
+++ view/estudiantes/crear.php
@@ -4,7 +4,7 @@
 ?>
 <div class="container-fluid" id="contendorprincipal">    
     <h1><?php echo $this->mensaje;?></h1>
-    <form class="form-control" action="<?php echo constant('URL'); ?>estudiantes/insertarEstudiantes" method="POST">
+    <form class="form-control" action="<?php echo constant('URL'); ?>estudiantes/insertarEstudiante" method="POST">
         <?php require 'form.php'; ?>
     </form>
 </div>
